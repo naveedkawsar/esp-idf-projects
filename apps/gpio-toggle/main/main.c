d5a21780a0484533e7dd926228d8414afaaaed5d
Naveed Kawsar <naveedkawsar@outlook.com>
2026-02-22 21:37:42 -0500

    Updated basic example to match closely with ESP-IDF best practices for error handling, debug prints

--- apps/gpio-toggle/main/main.c
+++ apps/gpio-toggle/main/main.c
@@ -1,34 +1,41 @@
 // Created using Shawn Hymel's excellent ESP-IDF tutorial:
 // https://shawnhymel.com/2872/esp32-getting-started-with-esp-idf/
 // https://github.com/ShawnHymel/course-iot-with-esp-idf/
-
-#include <stdio.h>
+// See also Low Level Learning's in-depth Getting Started video:
+// https://www.youtube.com/watch?v=dOVjb2wXI84
 
 #include "driver/gpio.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
+
+#define SLEEP_TIME_MS (1000u)
 
 void app_main(void)
 {
-    // Settings
-    const gpio_num_t gpio_pin = GPIO_NUM_4;
-    const uint32_t sleep_time_ms = 1000;
+    esp_err_t esp_ret;
     uint8_t gpio_state = 0;
 
+    char *current_taskname = pcTaskGetName(NULL);
+
     // Configure the GPIO
-    gpio_reset_pin(gpio_pin);
-    gpio_set_direction(gpio_pin, GPIO_MODE_OUTPUT);
+    esp_ret = gpio_reset_pin(GPIO_NUM_4);
+    ESP_ERROR_CHECK(esp_ret);
+
+    esp_ret = gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    ESP_ERROR_CHECK(esp_ret);
 
     // Superloop
     while (1) {
 
         // Toggle the GPIO
         gpio_state ^= 0x01;
-        gpio_set_level(gpio_pin, gpio_state);
+        esp_ret = gpio_set_level(GPIO_NUM_4, gpio_state);
+        ESP_ERROR_CHECK(esp_ret);
 
         // Print GPIO state
-        printf("GPIO state: %d\n", gpio_state);
+        ESP_LOGI(current_taskname, "GPIO state: %d\n", gpio_state);
 
         // Delay
-        vTaskDelay(sleep_time_ms / portTICK_PERIOD_MS); 
+        vTaskDelay(SLEEP_TIME_MS / portTICK_PERIOD_MS); 
     }
 }
